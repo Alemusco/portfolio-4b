5206cf1a18e114bbc728cf7a255208e8023c3453
Studente <muscolinoalessandro05@gmail.com>
2023-02-16 12:08:20 +0100

    Secondo Commit!

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
 
+const TitoloCorso = ({nome,anno})=>{
+  console.log('Nome: ',nome);
+  console.log('Anno: ',anno);
+  return(
+    <>
+    <h1>{`Corso ${nome} ${anno}`}</h1>
+    </>
+  )
+}
+
 function App() {
+  const corsi = [{
+    'id': 1,
+    'nome': 'React',
+    'anno': '2k23'
+  },
+  {
+    'id': 2,
+    'nome': 'Flutter',
+    'anno': '2k22'
+  },
+  {
+    'id': 3,
+    'nome': 'Angular',
+    'anno' : '2k24'
+  }]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        corsi.map((corso)=>{
+          return <TitoloCorso nome={corso.nome} anno ={corso.anno}/> 
+        })
+      }
+      
     </div>
   );
 }
